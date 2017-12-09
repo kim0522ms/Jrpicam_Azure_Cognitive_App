72fc290ec46371d3d2a2d0d301d17e6eaa5d8967
MS Kim <33797412+kim0522ms@users.noreply.github.com>
2017-12-09 23:37:08 +0900

    Update Main.java

--- PicameraTest01/src/Main.java
+++ PicameraTest01/src/Main.java
@@ -37,8 +37,8 @@ import com.hopding.jrpicam.exceptions.FailedToRunRaspistillException;
 
 public class Main {
 	
-	public static final String uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr";
-	public static final String subscriptionKey = "a1678fc3ebca45728db18c525bcad6a0";
+	public static final String uriBase = "<Your Azure Cognitive API URI>";
+	public static final String subscriptionKey = "<Your Azure Subscription Key>";
 	
 	public static void main(String[] args) {
 		try {
@@ -95,7 +95,7 @@ public class Main {
 			e.printStackTrace();
 		}
 		
-		// √‘øµ«— Snapshot ∆ƒ¿œ¿ª Binary Data∑Œ πŸ≤„º≠ Azure º≠πˆø° ¿¸º€
+		// Ï¥¨ÏòÅÌïú Snapshot ÌååÏùºÏùÑ Binary DataÎ°ú Î∞îÍøîÏÑú Azure ÏÑúÎ≤ÑÏóê Ï†ÑÏÜ°
 		HttpClient httpClient = new DefaultHttpClient();
 		
 		try
@@ -121,11 +121,11 @@ public class Main {
 			request.setEntity(entities);
 			System.out.println("executing request " + request.getRequestLine());
 			
-            // JSON «¸Ωƒ¿∏∑Œ ∞·∞˙∞™ ¿¸¥ﬁπﬁ¿Ω
+            // JSON ÌòïÏãùÏúºÎ°ú Í≤∞Í≥ºÍ∞í Ï†ÑÎã¨Î∞õÏùå
             HttpResponse response = httpClient.execute(request);
             HttpEntity entity = response.getEntity();
 
-            // Responseø° Entity∞° ¡§ªÛ¿˚¿∏∑Œ ¿‘∑¬µ«æ˙¿ª ∞ÊøÏ
+            // ResponseÏóê EntityÍ∞Ä Ï†ïÏÉÅÏ†ÅÏúºÎ°ú ÏûÖÎ†•ÎêòÏóàÏùÑ Í≤ΩÏö∞
             if (entity != null)
             {
                 String jsonString = EntityUtils.toString(entity);
@@ -156,320 +156,6 @@ public class Main {
 		{
 			System.out.println(e.getMessage());
 		}
-		
-		/*
-		try(ServerSocket server = new ServerSocket()){
-            // º≠πˆ √ ±‚»≠
-            InetSocketAddress ipep = new InetSocketAddress(9999);
-            server.bind(ipep);
-             
-            System.out.println("Waiting For Client...");
-             
-            //LISTEN ¥Î±‚
-            Socket client = server.accept();
-            System.out.println("Connection");
-             
-            // send,reciever Ω∫∆Æ∏≤ πﬁæ∆ø¿±‚
-            // ¿⁄µø close
-            try(OutputStream sender = client.getOutputStream();
-                InputStream reciever = client.getInputStream();){
-            	
-                //≈¨∂Û¿Ãæ∆Æ∑Œ∫Œ≈Õ ∏ﬁΩ√¡ˆ πﬁ±‚
-                byte[] data = new byte[11];
-                reciever.read(data, 0, data.length);
-                 
-                //ºˆΩ≈ ∏ﬁΩ√¡ˆ √‚∑¬
-                String message = new String(data);
-                message = message.trim();
-                
-                // ≈¨∂Û¿Ãæ∆Æ∑Œ∫Œ≈Õ πﬁ¿∫ ∏ﬁºº¡ˆ∞° √‘øµ¿œ ∞ÊøÏ
-                if (message.equals("shot"))
-                {
-                	
-                	// TODO : √‘øµ ∞¸∑√ ƒ⁄µÂ ¿€º∫ 
-                	try {
-            			RPiCamera piCamera = new RPiCamera("/home/pi/appcenterTest/Pictures");
-            			
-            			piCamera.setWidth(640).setHeight(480)
-            		    .setBrightness(50)                // Adjust Camera's brightness setting.
-            		    .setDRC(DRC.OFF)
-            		    .setExposure(Exposure.AUTO)       // Set Camera's exposure.
-            		    .setQuality(75)
-            		    .setTimeout(2)                    // Set Camera's timeout.
-            		    .setAddRawBayer(true);
-            			
-            			//piCamera.takeStill("Image3.jpg");
-            			
-            			BufferedImage image = ImageIO.read(piCamera.takeStill("capture.jpg"));
-            			
-            			File compressedImageFile = new File("/home/pi/appcenterTest/Pictures/compressedImage.jpg");
-            			OutputStream os = new FileOutputStream(compressedImageFile);
-            			
-            			Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
-            			
-            			ImageWriter writer = (ImageWriter) writers.next();
-            			ImageOutputStream ios = ImageIO.createImageOutputStream(os);
-            			
-            			writer.setOutput(ios);
-            			
-            			ImageWriteParam param = writer.getDefaultWriteParam();
-            			param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-            			param.setCompressionQuality(0.5f);
-            			
-            			writer.write(null, new IIOImage(image, null, null), param);
-            			os.close();
-            			ios.close();
-            			writer.dispose();
-            			
-            			//ImageIO.write(image, "jpg", new File("Image4.gif"));
-            			
-
-            		} catch (FailedToRunRaspistillException e) {
-            			// TODO Auto-generated catch block
-            			e.printStackTrace();
-            		} catch (IOException e) {
-            			// TODO Auto-generated catch block
-            			e.printStackTrace();
-            		} catch (InterruptedException e) {
-            			// TODO Auto-generated catch block
-            			e.printStackTrace();
-            		}
-            		
-            		// √‘øµ«— Snapshot ∆ƒ¿œ¿ª Binary Data∑Œ πŸ≤„º≠ Azure º≠πˆø° ¿¸º€
-            		HttpClient httpClient = new DefaultHttpClient();
-            		
-            		try
-            		{
-            			URIBuilder uriBuilder = new URIBuilder(uriBase);
-            			
-            			uriBuilder.setParameter("language", "unk");
-            			uriBuilder.setParameter("detectOrientation", "true");
-            			
-            			URI uri = uriBuilder.build();
-            			
-            			HttpPost request = new HttpPost(uri);
-            			
-                        request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-            			
-            			File file = new File("/home/pi/appcenterTest/Pictures/compressedImage.jpg");
-            			
-            			HttpEntity entities = MultipartEntityBuilder.create()
-            					.setMode(HttpMultipartMode.BROWSER_COMPATIBLE)
-            					.addBinaryBody("file", file)
-            					.build();
-            			
-            			request.setEntity(entities);
-            			System.out.println("executing request " + request.getRequestLine());
-            			
-                        // JSON «¸Ωƒ¿∏∑Œ ∞·∞˙∞™ ¿¸¥ﬁπﬁ¿Ω
-                        HttpResponse response = httpClient.execute(request);
-                        HttpEntity entity = response.getEntity();
-
-                        // Responseø° Entity∞° ¡§ªÛ¿˚¿∏∑Œ ¿‘∑¬µ«æ˙¿ª ∞ÊøÏ
-                        if (entity != null)
-                        {
-                            String jsonString = EntityUtils.toString(entity);
-                            JSONObject json = new JSONObject(jsonString);
-                            
-                            JSONArray jsonArr = new JSONArray();
-                            jsonArr.put("35.168647");
-                            jsonArr.put("128.994492");
-                            
-                            json.put("coordinate", jsonArr);
-                            
-                            System.out.println("REST Response:\n");
-                            System.out.println(json.toString(2));
-                            
-                            HttpClient json_httpClient = HttpClientBuilder.create().build(); //Use this instead
-                            
-                            HttpPost json_request = new HttpPost("http://172.17.19.49:8081/Silla_Server/op/JSON");
-                            
-                            StringEntity params =new StringEntity(json.toString());
-                            json_request.addHeader("content-type", "application/json");
-                            
-                            json_request.setEntity(params);
-                            
-                            HttpResponse json_response = json_httpClient.execute(json_request);
-                        }
-            		}
-            		catch (Exception e)
-            		{
-            			System.out.println(e.getMessage());
-            		}
-                }
-                else if (message.equals("shutdown"))
-                {
-                	// TODO : «¡∑Œ±◊∑• ¡æ∑·
-                	System.out.println("Recieved Message : " + message);
-                	System.out.println("Exit Program...");
-                	return;
-                }
-                else
-                {
-                	// TODO : øπø‹√≥∏Æ
-                	System.out.println("Unknown Message : " + message);
-                }
-            }
-        }catch(Throwable e){
-            e.printStackTrace();
-    }
-    */
-		/*
-		try(ServerSocket server = new ServerSocket()){
-            // º≠πˆ √ ±‚»≠
-            InetSocketAddress ipep = new InetSocketAddress(10500);
-            server.bind(ipep);
-             
-            System.out.println("Waiting For Client...");
-             
-            //LISTEN ¥Î±‚
-            Socket client = server.accept();
-            System.out.println("Connectted From Client!");
-            
-            // send,reciever Ω∫∆Æ∏≤ πﬁæ∆ø¿±‚
-            // ¿⁄µø close
-            try(OutputStream sender = client.getOutputStream();
-                InputStream reciever = client.getInputStream();){
-            	
-                //≈¨∂Û¿Ãæ∆Æ∑Œ∫Œ≈Õ ∏ﬁΩ√¡ˆ πﬁ±‚
-                byte[] data = new byte[11];
-                reciever.read(data, 0, data.length);
-                 
-                //ºˆΩ≈ ∏ﬁΩ√¡ˆ √‚∑¬
-                String message = new String(data);
-                message = message.trim();
-                
-                // ≈¨∂Û¿Ãæ∆Æ∑Œ∫Œ≈Õ πﬁ¿∫ ∏ﬁºº¡ˆ∞° √‘øµ¿œ ∞ÊøÏ
-                if (message.equals("shot"))
-                {
-                	// TODO : √‘øµ ∞¸∑√ ƒ⁄µÂ ¿€º∫ 
-                	try {
-            			RPiCamera piCamera = new RPiCamera("/home/pi/appcenterTest/Pictures");
-            			
-            			piCamera.setWidth(1280).setHeight(720)
-            		    .setBrightness(50)                // Adjust Camera's brightness setting.
-            		    .setDRC(DRC.OFF)
-            		    .setExposure(Exposure.AUTO)       // Set Camera's exposure.
-            		    .setQuality(75)
-            		    .setTimeout(2)                    // Set Camera's timeout.
-            		    .setAddRawBayer(true);
-            			
-            			//piCamera.takeStill("Image3.jpg");
-            			
-            			BufferedImage image = ImageIO.read(piCamera.takeStill("capture.jpg"));
-            			
-            			File compressedImageFile = new File("/home/pi/appcenterTest/Pictures/compressedImage.jpg");
-            			OutputStream os = new FileOutputStream(compressedImageFile);
-            			
-            			Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
-            			
-            			ImageWriter writer = (ImageWriter) writers.next();
-            			ImageOutputStream ios = ImageIO.createImageOutputStream(os);
-            			
-            			writer.setOutput(ios);
-            			
-            			ImageWriteParam param = writer.getDefaultWriteParam();
-            			param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-            			param.setCompressionQuality(0.5f);
-            			
-            			writer.write(null, new IIOImage(image, null, null), param);
-            			os.close();
-            			ios.close();
-            			writer.dispose();
-            			
-            			//ImageIO.write(image, "jpg", new File("Image4.gif"));
-            			
-
-            		} catch (FailedToRunRaspistillException e) {
-            			// TODO Auto-generated catch block
-            			e.printStackTrace();
-            		} catch (IOException e) {
-            			// TODO Auto-generated catch block
-            			e.printStackTrace();
-            		} catch (InterruptedException e) {
-            			// TODO Auto-generated catch block
-            			e.printStackTrace();
-            		}
-            		
-            		// √‘øµ«— Snapshot ∆ƒ¿œ¿ª Binary Data∑Œ πŸ≤„º≠ Azure º≠πˆø° ¿¸º€
-            		HttpClient httpClient = new DefaultHttpClient();
-            		
-            		try
-            		{
-            			URIBuilder uriBuilder = new URIBuilder(uriBase);
-            			
-            			uriBuilder.setParameter("language", "unk");
-            			uriBuilder.setParameter("detectOrientation", "true");
-            			
-            			URI uri = uriBuilder.build();
-            			
-            			HttpPost request = new HttpPost(uri);
-            			
-                        request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-            			
-            			File file = new File("/home/pi/appcenterTest/Pictures/compressedImage.jpg");
-            			
-            			HttpEntity entities = MultipartEntityBuilder.create()
-            					.setMode(HttpMultipartMode.BROWSER_COMPATIBLE)
-            					.addBinaryBody("file", file)
-            					.build();
-            			
-            			request.setEntity(entities);
-            			System.out.println("executing request " + request.getRequestLine());
-            			
-                        // JSON «¸Ωƒ¿∏∑Œ ∞·∞˙∞™ ¿¸¥ﬁπﬁ¿Ω
-                        HttpResponse response = httpClient.execute(request);
-                        HttpEntity entity = response.getEntity();
-
-                        // Responseø° Entity∞° ¡§ªÛ¿˚¿∏∑Œ ¿‘∑¬µ«æ˙¿ª ∞ÊøÏ
-                        if (entity != null)
-                        {
-                            String jsonString = EntityUtils.toString(entity);
-                            JSONObject json = new JSONObject(jsonString);
-                            
-                            JSONArray jsonArr = new JSONArray();
-                            jsonArr.put("35.168647");
-                            jsonArr.put("128.994492");
-                            
-                            json.put("coordinate", jsonArr);
-                            
-                            System.out.println("REST Response:\n");
-                            System.out.println(json.toString(2));
-                            
-                            HttpClient json_httpClient = HttpClientBuilder.create().build(); //Use this instead
-                            
-                            HttpPost json_request = new HttpPost("http://192.168.43.7:8081/Silla_Server/op/JSON");
-                            
-                            StringEntity params =new StringEntity(json.toString());
-                            json_request.addHeader("content-type", "application/json");
-                            
-                            json_request.setEntity(params);
-                            
-                            HttpResponse json_response = json_httpClient.execute(json_request);
-                        }
-            		}
-            		catch (Exception e)
-            		{
-            			System.out.println(e.getMessage());
-            		}
-                }
-                else if (message.equals("shutdown"))
-                {
-                	// TODO : «¡∑Œ±◊∑• ¡æ∑·
-                	System.out.println("Recieved Message : " + message);
-                	System.out.println("Exit Program...");
-                	return;
-                }
-                else
-                {
-                	// TODO : øπø‹√≥∏Æ
-                	System.out.println("Unknown Message : " + message);
-                }
-            }
-        }catch(Throwable e){
-            e.printStackTrace();
-    }
-    */
 	}
 
 }
